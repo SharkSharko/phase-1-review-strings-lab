73ba08f322371728375bd69832212dde646be6b3
SharkSharko <melileiemmanuel@gmail.com>
2025-03-12 18:40:11 +0300

    Completed Flatbook greeting lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+// Define the current user
+const currentUser = 'Emmanuel Melilei';
+
+// Create a welcome message using string interpolation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Convert the welcome message to uppercase for excitement
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create a short greeting using only the first initial of the current user
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
